9f7410c74de1ef1037d9d827e634db436ede5c8e
diegoall <dposadallano@gmail.com>
2021-09-14 20:59:57 -0500

    Uso de funciones y funciones anónimas

--- src/main.go
+++ src/main.go
@@ -2,35 +2,46 @@ package main
 
 import "fmt"
 
+func normalFunction() {
+	fmt.Println("Hola mundo")
+}
+
+func normalFunction2(message string) {
+	fmt.Println(message)
+}
+
+func tripleArgument(a, b int, c string) {
+	fmt.Println(a, b, c)
+}
+
+func returnValue(a int) int {
+	return a * 2
+}
+
+func doubleReturn(a int) (c, d int) {
+	return a, a * 2
+
+}
+
 func main() {
 
-	//Funciones mas importantes del paquete fmt
+	//Invocar la funciión
 
-	//Declaración de variables
-	helloMessage := "Hello"
-	worldMessage := "world"
+	/*fmt.Println("Hola mundo")
+	fmt.Println("Hola mundo 2")
+	fmt.Println("Hola mundo 3")*/
 
-	//Println
-	fmt.Println(helloMessage, worldMessage)
-	fmt.Println(helloMessage, worldMessage)
+	normalFunction()
+	normalFunction2("Que mas pues parcero")
+	tripleArgument(1, 3, "guineos")
 
-	// Printf
-	expresion := "Parcero"
-	cantidad := 500
-	fmt.Printf("%s por favor me da %d de cilantro\n", expresion, cantidad)
+	value := returnValue(3)
+	fmt.Println("Value:", value)
 
-	//Cuando no se sabe el tipo de valor, la buena práctica es intentar asignar el tipo de dato al que pertenece
-	fmt.Printf("%v por favor me da %v de cilantro\n", expresion, cantidad)
+	value1, value2 := doubleReturn(2)
+	fmt.Println("value1 y value2:", value1, value2)
 
-	//Sprintf va a generar un string pero no lo va a imprimir en consola pero lo va a guardar como un string
-	message := fmt.Sprintf("%v por favor me da %v de cilantro", expresion, cantidad)
-	fmt.Println(message)
-
-	//Tipo de datos
-	fmt.Printf("helloMessage: %T\n", helloMessage)
-
-	fmt.Printf("Cantidad: %T\n", cantidad)
-
-	//Las anteriores son las funciones mas "importantes" o útiles del paquete fmt.
+	value3, _ := doubleReturn(3)
+	fmt.Println("value1", value3)
 
 }
