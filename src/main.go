eb0298c1f2cda0e2f271acba4d4319e7b0c28134
diegoall <dposadallano@gmail.com>
2021-09-16 00:57:56 -0500

    Operadores lógicos y de comparación

--- src/main.go
+++ src/main.go
@@ -2,53 +2,45 @@ package main
 
 import "fmt"
 
+func igualdad(v1, v2 bool) bool {
+	return v1 == v2
+}
+
+func distinto(v1, v2 bool) bool {
+	return v1 != v2
+}
+
+func comparacion(i1, i2 int) (ans1, ans2, ans3, ans4 bool) {
+	return i1 > i2, i1 < i2, i1 >= i2, i1 <= i2
+}
+
+func logicGates(v1, v2 bool) (ans1, ans2, ans3 bool) {
+	return v1 && v2, v1 || v2, !v1
+}
+
 func main() {
 
-	//For condicional
-	fmt.Println("For condicional")
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-	}
+	fmt.Println("Operadores lógicos y de comparación")
 
-	fmt.Println()
+	var valor1 bool = false
+	var valor2 bool = true
+	var entero1 int = 5
+	var entero2 int = -5
 
-	//For while
-	fmt.Println("For while")
-	counter := 0
-	for counter < 10 {
-		fmt.Println(counter)
-		counter++
-	}
+	eq := igualdad(valor1, valor2)
+	ds := distinto(valor1, valor2)
 
-	fmt.Println()
+	comp1, comp2, comp3, comp4 := comparacion(entero1, entero2)
+	andGate, orGate, notGate := logicGates(valor1, valor2)
 
-	//For condicional (decremento)
-	for i := 10; i > 0; i-- {
-		fmt.Println(i)
+	fmt.Printf("%t == %t : %t\n", valor1, valor2, eq)
+	fmt.Printf("%t != %t : %t\n", valor1, valor2, ds)
+	fmt.Printf("%d > %d : %t\n", entero1, entero2, comp1)
+	fmt.Printf("%d < %d : %t\n", entero1, entero2, comp2)
+	fmt.Printf("%d >= %d : %t\n", entero1, entero2, comp3)
+	fmt.Printf("%d <= %d : %t\n", entero1, entero2, comp4)
+	fmt.Printf("%t && %t : %t\n", valor1, valor2, andGate)
+	fmt.Printf("%t || %t : %t\n", valor1, valor2, orGate)
+	fmt.Printf("!%t : %t\n", valor1, notGate)
 
-	}
-
-	fmt.Println()
-
-	//For Range: Cuando se tiene una colección de un objeto.
-	listaNumerosPares := []int{2, 4, 6, 8, 10, 12, 14, 16, 18, 20}
-
-	for i, par := range listaNumerosPares {
-		fmt.Printf("posición %d número par: %d \n", i, par)
-
-	}
-
-	//For forever
-	/*
-		counterForever := 0
-		for {
-			fmt.Println(counterForever)
-			counterForever++
-		}
-	*/
-	/*Nota: Tener en cuenta que en Golang solo existe unciclo iterativo que es "For"-
-	En otros lenguajes tienen otros ciclos como For, While, Do While, etc
-	Tener en cuenta que enc asod e utilizar el ciclo For forever, debe haber una manera que dentro del
-	bloque de código pueda salir del ciclo for, de lo contrario sera un gasto inoficioso de recursos y deberá
-	detenerse es ciclo de forma manual. */
 }
