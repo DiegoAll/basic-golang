abbc64fb506a77ebfe29f7fb516970baa3a89f06
diegoall <dposadallano@gmail.com>
2021-09-17 07:41:58 -0500

    With oddOrEven and validatePassword function

--- src/main.go
+++ src/main.go
@@ -1,6 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func oddOrEvent(num int) string {
+	if num%2 == 0 {
+		return "El número es par"
+	}
+	return "El número es impar"
+}
+
+func validatepassword(pw string) {
+	pwTrue := "JoseNest0rPekerman"
+
+	if pw != pwTrue {
+		fmt.Println("Acceso denegado. Contraseña inválida")
+	} else {
+		fmt.Println("Acceso autorizado. Bienvenido")
+	}
+}
 
 func main() {
 
@@ -23,4 +44,15 @@ func main() {
 		fmt.Println("Es verdad, OR")
 	}
 
+	numeroPar := 8
+	numeroImpar := 33
+
+	fmt.Println(oddOrEvent(numeroPar))
+	fmt.Println(oddOrEvent(numeroImpar))
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	password := scanner.Text()
+
+	validatepassword(password)
 }
