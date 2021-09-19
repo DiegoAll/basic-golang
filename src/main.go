8136e33ca6145abfe3d483d07abbc16c292cc4de
diegoall <dposadallano@gmail.com>
2021-09-19 00:22:58 -0500

    Llave valor con maps

--- src/main.go
+++ src/main.go
@@ -1,66 +1,24 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func isPalindromo(text string) {
-	var textReverse string
-	for i := len(text) - 1; i >= 0; i-- {
-		textReverse += string(text[i]) // Se realiza una conversion a string ya que retorna un tipo de dato byte correspondiente al codigo ascii del caracter
-	}
-
-	if text == textReverse {
-		fmt.Println("Es palíndromo")
-	} else {
-		fmt.Println("No es palíndromo")
-	}
-}
-
-/*Ejemplos de palindromos
--Anita lava la tina
--Amor a Roma
--Allí ves a Sevilla
--Amigo no gima
--Amad a la dama
--Ella te da detalle
--Anula la luna
--Isaac no ronca asi
--Ligar es ser agil
--Luz azul
--Oso baboso
--Se van sus naves
--Sometamos o matemos
--Ojo rojo
--Son mulas o civicos alumnos
--Son robos o sobornos
--Amor a roma
-*/
+import "fmt"
 
 func main() {
+	m := make(map[string]int)
 
-	slice := []string{"Que", "mas", "pues"}
+	m["Faustino"] = 45
+	m["Justino"] = 24
 
-	for i, valor := range slice {
-		fmt.Println(i, valor)
+	fmt.Println(m)
+
+	//recorrer el map
+	/* Tener en cuenta que al realizar el recorrido con map se hace de forma aleatoria, no va a retornar la clave y valor en el mismo
+	orden en el cual fue ingresad, como sucede de forma concurrente va a ejecutarse de forma aleatoria, Si se requiere la información en el mismo orden se
+	recomiendan los slices */
+	for i, v := range m {
+		fmt.Println(i, v)
 	}
 
-	fmt.Println()
-
-	//Sino me interesa el indice, solamente el valor, lo escapamos.
-	for _, valor := range slice {
-		fmt.Println(valor)
-	}
-	palabra := "amigonogima"
-	isPalindromo(palabra)
-
-	//Case insensitive
-	//Ama
-	var palabra2 string
-	fmt.Scan(&palabra2)
-	word := strings.ToLower(palabra2)
-
-	isPalindromo(word)
-
+	//Como obtener uno de los valores
+	value, ok := m["Justino"]
+	fmt.Println(value, ok)
 }
