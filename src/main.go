d9b97bdc8e038a358ddc38ea49cb952b57c03bd4
diegoall <dposadallano@gmail.com>
2021-09-19 17:48:24 -0500

    Personalizar el output en consola de un struct

--- src/main.go
+++ src/main.go
@@ -4,44 +4,16 @@ import "fmt"
 
 type pc struct {
 	ram   int
-	disk  int
 	brand string
+	disk  int
 }
 
-func (myPC pc) ping() {
-	fmt.Println(myPC.brand, "Pong")
-}
-
-func (myPC *pc) duplicateRAM() {
-	myPC.ram = myPC.ram * 2
-}
+//Función para personalizar el output de consola.
+/*func (myPC pc) String() string {
+	return fmt.Sprintf("Tengo %d GB RAM, %d GB Disco y es una %s", myPC.ram, myPC.disk, myPC.brand)
+}*/
 
 func main() {
-	a := 50
-	b := &a
-
-	fmt.Println(b)
-	fmt.Println(*b)
-
-	*b = 100
-	fmt.Println(a)
-
-	myPC := pc{ram: 16, disk: 200, brand: "msi"}
+	myPC := pc{ram: 16, brand: "msi", disk: 100}
 	fmt.Println(myPC)
-
-	myPC.ping()
-
-	fmt.Println()
-
-	/* Suponiendo que yo quiera modificar la RAM podria utilizar la instrucción  myPC.ram = 20
-	Sin embargo a nivel de programación, a gran escala y teniendo mucho codigo. esto incluso puede llegar a ser redundante.
-	Entonces se utiliza la siguiente técnica para optimizar. Crear una funcion como la siguiente: "duplicateRAM"
-	*/
-	fmt.Println("Actual ==>", myPC)
-
-	myPC.duplicateRAM()
-	fmt.Println("Después de la primera invocación ==>", myPC)
-
-	myPC.duplicateRAM()
-	fmt.Println("Después de la segunda invocación ==>", myPC)
 }
